e8819514913fd4a0f12aa0e127794e280f8cf19e
mahmoudshaddad <85461844+mahmoudshaddad@users.noreply.github.com>
2022-07-27 23:54:03 +0200

    all working

--- Revitgridpulgin/Application.cs
+++ Revitgridpulgin/Application.cs
@@ -22,39 +22,41 @@ namespace Revitgridpulgin
 
 
         #region from Youtube 
-      
-            const string ribbon_tab = "PlanMacher";
-            const string ribbon_Pannel = "Create a Gird";
-            public Result OnStartup(UIControlledApplication application)
+
+        const string ribbon_tab = "PlanMacher";
+        const string ribbon_Pannel = "Create a Gird";
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // get the ribbon tab
+            try
             {
-                // get the ribbon tab
-                try
-                {
-                    application.CreateRibbonTab(ribbon_tab);
-                }
-                catch (Exception) { TaskDialog.Show("title there is a problem"
-                    ,
-                    " Line 35" );
-                    } //tab is already exists
+                application.CreateRibbonTab(ribbon_tab);
+            }
+            catch (Exception)
+            {
+                TaskDialog.Show("title there is a problem"
+                ,
+                " Line 35");
+            } //tab is already exists
 
-                // get or Create the panel 
-                RibbonPanel pannel = null;
+            // get or Create the panel 
+            RibbonPanel pannel = null;
 
-                List<RibbonPanel> panels = application.GetRibbonPanels(ribbon_tab);
+            List<RibbonPanel> panels = application.GetRibbonPanels(ribbon_tab);
 
-                foreach (RibbonPanel pnl in panels)
+            foreach (RibbonPanel pnl in panels)
+            {
+                if (pnl.Name == ribbon_Pannel)
                 {
-                    if (pnl.Name == ribbon_Pannel)
-                    {
-                        pannel = pnl;
-                        break;
-                    }
+                    pannel = pnl;
+                    break;
                 }
-                // couldnot find the pannel. create it 
-                if (pannel == null)
-                {
-                    pannel = application.CreateRibbonPanel(ribbon_tab, ribbon_Pannel);
-                }
+            }
+            // couldnot find the pannel. create it 
+            if (pannel == null)
+            {
+                pannel = application.CreateRibbonPanel(ribbon_tab, ribbon_Pannel);
+            }
 
             // get the image for the Button 
             Image img = Properties.Resources.icons8_fire_extinguisher_ico;
@@ -66,45 +68,45 @@ namespace Revitgridpulgin
                 "Create a Gird",
                 Assembly.GetExecutingAssembly().Location,
                 " Revitgridpulgin.RevitPlanMaker")
-                {
-                    ToolTip = "Short Description that show when you hover over the button",
-                    LongDescription = "Longer description show when you hober over the button for few sec ",
+            {
+                ToolTip = "Short Description that show when you hover over the button",
+                LongDescription = "Longer description show when you hober over the button for few sec ",
                 Image = imgSrc,
                 LargeImage = imgSrc
             };
-                
-                // add your button 
-                PushButton button = pannel.AddItem(btnData) as PushButton;
-                button.Enabled = true;
-                button.
-                return Result.Succeeded;
-            }
 
-            public Result OnShutdown(UIControlledApplication application)
-            {
-                return Result.Succeeded;
-            }
-            private BitmapSource GetImageSource(Image img)
-            {
-                BitmapImage bmp = new BitmapImage();
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    img.Save(ms, ImageFormat.Png);
-                    ms.Position = 0;
+            // add your button 
+            PushButton button = pannel.AddItem(btnData) as PushButton;
+            button.Enabled = true;
 
-                    bmp.BeginInit();
-                    bmp.CacheOption = BitmapCacheOption.OnLoad;
-                    bmp.UriSource = null;
-
-                    bmp.StreamSource = ms;
-                    bmp.EndInit();
-
-                }
-                return bmp;
-
-            }
+            return Result.Succeeded;
         }
-        #endregion
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        private BitmapSource GetImageSource(Image img)
+        {
+            BitmapImage bmp = new BitmapImage();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Png);
+                ms.Position = 0;
+
+                bmp.BeginInit();
+                bmp.CacheOption = BitmapCacheOption.OnLoad;
+                bmp.UriSource = null;
+
+                bmp.StreamSource = ms;
+                bmp.EndInit();
+
+            }
+            return bmp;
+
+        }
     }
+    #endregion
+}
 
 
